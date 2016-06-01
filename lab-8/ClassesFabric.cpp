530be73293b164e4306bb4775a21176d186cbb43
AlexYshkevich <bor_bor_1997@mail.ru>
2016-06-01 23:11:01 +0300

    Update ClassesFabric.cpp

--- lab-8/ClassesFabric.cpp
+++ lab-8/ClassesFabric.cpp
@@ -1,130 +1,140 @@
-#include <map>
-#include <functional>
+#include "stdafx.h"
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <map>
 #include <sstream>
 #include <string>
-
 using namespace std;
-
-class Base {
-private:
+class Base 
+{
+protected:
 	int value;
 public:
-	Base() : value(0) {}
-	Base(int val) : value(val) {}
+	Base() {};
+	Base(int x) 
+	{
+		value = x;
+	}
 	virtual void show() = 0;
-	int get() { return value; }
-
 };
+class A : public Base 
+{
+public: 
+	A(int x) 
+	{
+		value = x;
+	};
+	void show() 
+	{
+		cout << "Class A: " << value << endl;
 
-class A : public Base {
-public:
-	A() : Base() {}
-	A(int val) : Base(val) {}
-	void show() {
-		cout << "class A: " << get() << endl;
 	}
 };
-
-class B : public Base {
+class B: public Base 
+{
 public:
-	B() : Base() {}
-	B(int val) : Base(val) {}
-	void show() {
-		cout << "class B: " << get() << endl;
+	B(int x) 
+	{
+		value = x;
+	};
+	void show() 
+	{
+		cout << "Class B: " << value << endl;
 	}
 };
-
-class C : public Base {
+class C : public Base 
+{
 public:
-	C() : Base() {}
-	C(int val) : Base(val) {}
-	void show() {
-		cout << "class C: " << get() << endl;
+	C(int x) 
+	{
+		value = x;
+	};
+	void show() 
+	{
+		cout << "Class C: " << value << endl;
 	}
 };
+vector<Base*> my_obj;
+class Functor
+{
+public:
+	virtual void operator()() {};
+	virtual void operator()(string, int) {};
+};
 
-vector<string> &split(const string &s, char delim, vector<string> &elems) {
+class FunctorShow : public Functor
+{
+public:
+	FunctorShow() 
+	{
+
+	};
+	void operator()() {
+
+		for (auto it : my_obj)
+			it->show();
+	};
+};
+
+class FunctorCreate : public Functor
+{
+public:
+	FunctorCreate() {};
+	void operator()(string classname, int value) 
+	{
+		Base* obj;
+		if (classname == "A")
+			obj = new A(value);
+		if (classname == "B")
+			obj = new B(value);
+		if (classname == "C")
+			obj = new C(value);
+		my_obj.push_back(obj);
+	};
+};
+vector<string> split(const string &s, char delim)
+{
+	vector<string> elems;
 	stringstream ss(s);
 	string item;
-	while (getline(ss, item, delim)) {
+
+	while (getline(ss, item, delim))
 		elems.push_back(item);
-	}
+
 	return elems;
 }
-
-vector<string> split(const string &s, char delim) {
-	vector<string> elems;
-	split(s, delim, elems);
-	return elems;
-}
-
-void showAll(vector<A> classA, vector<B> classB, vector<C> classC) {
-	for (auto it : classA) {
-		it.show();
-	}
-	for (auto it : classB) {
-		it.show();
-	}
-	for (auto it : classC) {
-		it.show();
-	}
-}
-
-int main() {
-	size_t N, size;
-	string command;
+int main()
+{
+	string com;
+	int n;
+	map<string, Functor*> mfunc;
+	Functor *f = new FunctorShow;
+	Functor *fc = new FunctorCreate;
+	mfunc["showall"] = f;
+	mfunc["create"] = fc;
+	Functor *fctr;
+	cout << endl;
+	cin >> n;;
 	vector<string> commands;
-	vector<A> classA;
-	vector<B> classB;
-	vector<C> classC;
-	cin >> N;
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	for (size_t i = 0; i < N; i++) {
-		getline(cin, command);
-		commands = split(command, ' ');
-		size = commands.size();
-		if (size == 3) {
-			if (commands[0] == "create") {
-				if (commands[1] == "A") {
-					A obj(stoi(commands[2]));
-					classA.push_back(obj);
-				}
-				else if (commands[1] == "B") {
-					B obj(stoi(commands[2]));
-					classB.push_back(obj);
-				}
-				else if (commands[1] == "C") {
-					C obj(stoi(commands[2]));
-					classC.push_back(obj);
-				}
-				else {
-					cout << "Incorrect command. Try again.\n";
-					i--;
-				}
-			}
-			else {
-				cout << "Incorrect command. Try again.\n";
-				i--;
-			}
-		}
-		else if (size == 1) {
-			if (commands[0] == "showall") {
-				showAll(classA, classB, classC);
-			}
-			else {
-				cout << "Incorrect command. Try again.\n";
-				i--;
-			}
-		}
-		else {
-			cout << "Incorrect command. Try again.\n";
-			i--;
+
+	for (int i = 1; i <= n; i++)
+	{
+		getline(cin, com);
+		commands = split(com, ' ');
+		fctr = mfunc[commands[0]];
+
+		if (commands.size() == 1)
+			(*fctr)();
+		else if (commands.size() == 3)
+			(*fctr)(commands[1], stoi(commands[2]));
+		else
+		{
+			cout << "Whoops...Try again!"; i--;
 		}
 	}
 
-	//showAll(classA, classB, classC);
 	system("pause");
 	return 0;
 }
